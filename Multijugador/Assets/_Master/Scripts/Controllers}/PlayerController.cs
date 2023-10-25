7a186d485b2f51a78fa5a021d7a5de185256aa78
LDom890 <montecinonestor890@gmail.com>
2023-10-24 18:41:25 -0600

    Add Multiple Players

--- Multijugador/Assets/_Master/Scripts/Controllers}/PlayerController.cs
+++ Multijugador/Assets/_Master/Scripts/Controllers}/PlayerController.cs
@@ -6,22 +6,51 @@ public class PlayerController : MonoBehaviour
     public float fuerzaSalto = 10.0f;
     private bool enElSuelo = true;
     private Rigidbody2D rb;
+    private int playerIndex;
+
+    private static int playerIndex2 = 0;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent < Rigidbody2D>();
+
+        if (playerIndex2 == 0)
+        {
+            playerIndex = 1;
+            playerIndex2 = 2;
+        }
+        else
+        {
+            playerIndex = playerIndex2;
+            playerIndex2++;
+        }
     }
 
     void Update()
     {
         // Movimiento horizontal
-        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoHorizontal = 0f;
+
+        if (playerIndex == 1)
+        {
+            movimientoHorizontal = Input.GetAxis("Horizontal");
+        }
+        else if (playerIndex == 2)
+        {
+            movimientoHorizontal = Input.GetAxis("Horizontal2");
+        }
+
         Vector2 velocidad = rb.velocity;
         velocidad.x = movimientoHorizontal * velocidadMovimiento;
         rb.velocity = velocidad;
 
         // Salto
-        if (enElSuelo && Input.GetKeyDown(KeyCode.Space))
+        if (enElSuelo && ((playerIndex == 1 && Input.GetKeyDown(KeyCode.Space)) || (playerIndex == 2 && Input.GetKeyDown(KeyCode.C))))
+        {
+            rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+            enElSuelo = false;
+        }
+        if (enElSuelo && ((playerIndex == 2 && Input.GetKeyDown(KeyCode.C)) || (playerIndex == 2 && Input.GetKeyDown(KeyCode.C))))
         {
             rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
             enElSuelo = false;
